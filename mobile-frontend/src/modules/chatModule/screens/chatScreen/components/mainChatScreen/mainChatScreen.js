6a9966a4c8cb375e7eef25067c5a2ee34b3cde67
Olololoy <akshatk128@gamil.com>
2023-07-24 13:26:32 +0530

    feat: message screen styling

--- mobile-frontend/src/modules/chatModule/screens/chatScreen/components/mainChatScreen/mainChatScreen.js
+++ mobile-frontend/src/modules/chatModule/screens/chatScreen/components/mainChatScreen/mainChatScreen.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import styles from './styles';
 
-const MainChatScreen = () => {
+const NonGroupText = (item, uId) => {
+  const sent = (uId === item.uId ? true : false);
+  return (<View style={{
+    ...styles.messageContainer.common,
+    ...(sent ? styles.messageContainer.sent : styles.messageContainer.received),
+  }}>
+    <Text style={styles.messageText}>
+      {item.message}
+    </Text>
+    <Text style={styles.timeText}>
+      {item.timeStamp}
+    </Text>
+  </View>
+  );
+}
+
+const GroupText = () => {
+  
+}
+
+
+const MainChatScreen = ({navigation}, uId) => {
+
+  const renderMessageItem = ({item}) => {
+    if(isGroup){
+
+    } else {
+      <NonGroupText uId={uId} item={item}/>
+    }
+    return null;
+  }
+
+  const onBackPress = () => {
+    navigation.goBack();
+  }
+
+
 
   return(
-    <View>
-      <Text>
-        Privee Chats Here
-      </Text>
+    <View style={styles.rootContainer}>
+      {/* <Header onBack={onBackPress}
+      /> */}
+      <FlatList
+      data={Data}
+      renderItem={renderMessageItem}
+      style={styles.flatlistContainer}
+      contentContainerStyle={styles.flatlistContentContainerStyles}
+      inverted
+      ListHeaderComponent
+      ListFooterComponent
+      />
+      {/* <Footer/> */}
     </View>
   );
 }
---
+++ mobile-frontend/src/modules/chatModule/screens/chatScreen/components/mainChatScreen/styles.js
@@ -0,0 +1,13 @@
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import {ms, mvs} from 'react-native-size-matters';
+import lightTheme from '../../../../../../themes/lightTheme';
+import cfs from '../../../../../../themes/fontStyles';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1, 
+    alignSelf: 'stretch',
+    alignItems: 'stretch',
+  },
+});
