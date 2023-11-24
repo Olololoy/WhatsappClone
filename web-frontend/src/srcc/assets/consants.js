7dcdf096c5bf1ba5a85b7070c5eeb691e911b9b2
Olololoy <akshatk128@gamil.com>
2023-11-24 16:32:32 +0530

    feat: resync anusha ne khel liya

---
+++ web-frontend/src/srcc/assets/consants.js
@@ -0,0 +1,27 @@
+export const  loremIpsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non pulvinar neque laoreet suspendisse interdum consectetur. Arcu bibendum at varius vel. Consectetur a erat nam at lectus urna duis. Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean. Odio eu feugiat pretium nibh ipsum consequat. Nibh cras pulvinar mattis nunc. Vulputate dignissim suspendisse in est ante in nibh. Cursus in hac habitasse platea dictumst quisque sagittis. Et pharetra pharetra massa massa ultricies mi. In massa tempor nec feugiat.'
+
+export const messages = {
+  singleText: [
+    {
+      id: '1',
+      from: 'akshat',
+      data: 'Hi aunty kaisi ho',
+      date: '24-11-2023',
+      time:  '14:20:34',
+    },
+    {
+      id: '2',
+      from: 'akshat',
+      data: 'Bologi kucch',
+      date: '24-11-2023',
+      time:  '14:20:38',
+    },
+    {
+      id: '3',
+      from: 'anusha',
+      data: 'bolo uncle',
+      date: '24-11-2023',
+      time:  '14:20:42',
+    },
+  ]
+}
--- web-frontend/src/srcc/screens/mainScreen/Components/activityWindow/activityWindow.js
+++ web-frontend/src/srcc/screens/mainScreen/Components/activityWindow/activityWindow.js
@@ -1,12 +1,25 @@
 // import React from 'react';
 import './activityWindow.css';
+import { loremIpsumText } from '../../../../assets/consants';
 
 function ActivityWindow() {
     return (
         <div className='activityWindow'>
-            hullo
+            <div className='scrollMessageDiv'>
+              {loremIpsumText}
+            </div>
+            <div className='bottomContainer'>
+                <input type='text' id='messageInputBox' placeholder='Message'/>
+            </div>
         </div>
     )
 }
 
 export default ActivityWindow;
+
+/*
+message sidebar and main activity screen push nahi hui hai 
+
+React jsx is different from pure html tags
+  clear example input
+*/
